21186b13aa836293372b5b009f34b361284caef2
thanh4890 <thanh4890@gmail.com>
2018-06-28 18:05:38 +0700

    Added redux form and reddit posts examples

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const About = () => (
-  <div>
-    <h1>About Page</h1>
-    <p>Did you get here via Redux?</p>
-  </div>
-);
+let AboutForm = props => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+AboutForm = reduxForm({
+  form: 'about'
+})(AboutForm);
+
+const About = () => {
+  const handleSubmit = values => {
+    console.log(values);
+  };
+
+  // https://github.com/erikras/redux-form/issues/1270#issuecomment-239636759
+  return (
+    <div>
+      <h1>About Page</h1>
+      <AboutForm onSubmit={handleSubmit} />
+    </div>
+  );
+};
 
 export default About;
--- src/modules/index.js
+++ src/modules/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import counter from './counter';
 import posts from './posts';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
   router: routerReducer,
   counter,
-  posts
+  posts,
+  form: formReducer
 });
