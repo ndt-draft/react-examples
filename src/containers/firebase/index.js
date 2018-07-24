a5c50050c3d943c160584ca28d8d061151a3d220
thanh4890 <thanh4890@gmail.com>
2018-07-24 21:01:48 +0700

    Demo simple chat app

--- src/containers/firebase/index.js
+++ src/containers/firebase/index.js
@@ -3,24 +3,77 @@ import { connect } from 'react-redux'
 import api from '../../modules/api'
 
 class Firebase extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      message: '',
+      messages: {}
+    }
+  }
+
   componentDidMount() {
+    let self = this
+
     // Initialize Firebase
     api.initializeApp()
 
-    var newUserKey = api.database
+    let messagesRef = api.database.ref('/messages')
+    messagesRef.on('child_added', data => {
+      let messages = { ...self.state.messages }
+      messages[data.key] = {
+        message: data.val().message
+      }
+      self.setState({
+        messages: messages
+      })
+    })
+
+    return api.database
+      .ref('/messages/')
+      .once('value')
+      .then(function(snapshot) {
+        let messages = snapshot.val() || []
+        self.setState({
+          messages
+        })
+      })
+  }
+
+  addMessage = e => {
+    e.preventDefault()
+
+    var newMessageKey = api.database
       .ref()
-      .child('users')
+      .child('messages')
       .push().key
 
-    api.database.ref(`/users/${newUserKey}`).set({
-      username: 'first user',
-      email: 'first@user.com',
-      profile_picture: 'http://placehold.it/50x50'
+    api.database.ref(`/messages/${newMessageKey}`).set({
+      message: this.state.message
+    })
+
+    this.setState({
+      message: ''
+    })
+  }
+
+  changeMessage = e => {
+    this.setState({
+      message: e.target.value
     })
   }
 
   render() {
-    return <div>Firebase</div>
+    return (
+      <div>
+        {Object.keys(this.state.messages).map(key => (
+          <div key={key}>{this.state.messages[key].message}</div>
+        ))}
+        <form onSubmit={this.addMessage}>
+          <input value={this.state.message} onChange={this.changeMessage} />
+          <button type="submit">Add message</button>
+        </form>
+      </div>
+    )
   }
 }
 
